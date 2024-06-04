31ed15d73d4aa699047bf5f249bf111d01d22c51
Ange Mukundente <135144334+M0111Ange@users.noreply.github.com>
2024-06-04 23:30:11 +0200

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,18 +1,3 @@
-const getListStudents = () => [
-  {
-    id: 1,
-    firstName: 'Guillaume',
-    location: 'San Francisco',
-  },
-  {
-    id: 2,
-    firstName: 'James',
-    location: 'Columbia',
-  },/**
- * Retrieves a list of students.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- * @returns {{id: Number, firstName: String, location: String}[]}
- */
 export default function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
@@ -20,11 +5,3 @@ export default function getListStudents() {
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
-  {
-    id: 5,
-    firstName: 'Serena',
-    location: 'San Francisco',
-  },
-];
-
-export default getListStudents;
